4b5f41ac1afe4cf91720aa4e9e1f831e55cd6acb
Pavlo Kotsan <paul@Pavlos-MacBook-Pro.local>
2024-12-22 15:03:44 -0500

    Fix Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,26 @@
 'use strict';
 
+function splitInput(strInput) {
+  const result = [];
+  let currentWorld = '';
+
+  for (let i = 0; i < strInput.length; i++) {
+    const char = strInput[i];
+
+    if (char === char.toUpperCase() && i !== 0) {
+      // Push first word and capitalize first character.
+      result.push(currentWorld[0].toUpperCase() + currentWorld.slice(1));
+      currentWorld = char;
+    } else {
+      currentWorld += char;
+    }
+  }
+
+  result.push(currentWorld[0].toUpperCase() + currentWorld.slice(1));
+
+  return result;
+}
+
 const inputs = document.querySelectorAll('form input');
 
 for (const input of inputs) {
@@ -7,8 +28,8 @@ for (const input of inputs) {
 
   label.classList.add('field-label');
   label.setAttribute('for', input.id);
-  label.textContent = input.name;
+  label.textContent = splitInput(String(input.name)).join(' ').toUpperCase();
 
-  input.placeholder = input.name;
+  input.placeholder = splitInput(String(input.name)).join(' ');
   input.parentNode.insertBefore(label, input);
 }
